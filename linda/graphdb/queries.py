acde539647d7cebdacd3a1e8320703f02c13c085
Michael Petychakis <mpetyx@gmail.com>
2014-05-26 17:29:19 +0300

    minor fix for the database path

--- linda/graphdb/queries.py
+++ linda/graphdb/queries.py
@@ -63,7 +63,7 @@ import dgraphdbstore
 
 
 graphdb = dgraphdbstore.DeepGraphStore()
-graphdb.open('deepGraphFile_321763af3ed32e1bb47233944ef6bc74_140512_191741')
+graphdb.open('graphdb/deepGraphFile_321763af3ed32e1bb47233944ef6bc74_140512_191741')
 
 results = graphdb.query(DETAILS_ABOUT_VOCABULARY)
 
