67e78bc8b26fca25f6e2d189319ed3b9e2c358ff
Dimitris Papaspyros <dipapaspyros@gmail.com>
2014-07-03 11:47:54 +0300

    Fixed models indentation.

--- linda/linda/models.py
+++ linda/linda/models.py
@@ -17,16 +17,16 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
 
     nameAppearance = models.CharField(max_length=256, blank=True,
-                                      null=True) #Information on the way the name is displayed in public (for instance, Dimitris Batis or Dimitris G. Batis or Batis Dimitris)
+                                      null=True)  #Information on the way the name is displayed in public (for instance, Dimitris Batis or Dimitris G. Batis or Batis Dimitris)
     country = models.CharField(max_length=3, default='--',
-                               choices=COUNTRIES) #combobox PREDEFINED LIST. LIST "COUNTRIES"
-    facebookUrl = models.URLField(max_length=256, blank=True, null=True) #User's Facebook profile
-    twitterUrl = models.URLField(max_length=256, blank=True, null=True) #User's Twitter profile
-    googleUrl = models.URLField(max_length=256, blank=True, null=True) #User's Google+ profile
-    linkedInUrl = models.URLField(max_length=256, blank=True, null=True) #User's LinkedIn profile
-    websiteUrl = models.URLField(max_length=256, blank=True, null=True) #User's personal website
+                               choices=COUNTRIES)  #combobox PREDEFINED LIST. LIST "COUNTRIES"
+    facebookUrl = models.URLField(max_length=256, blank=True, null=True)  #User's Facebook profile
+    twitterUrl = models.URLField(max_length=256, blank=True, null=True)  #User's Twitter profile
+    googleUrl = models.URLField(max_length=256, blank=True, null=True)  #User's Google+ profile
+    linkedInUrl = models.URLField(max_length=256, blank=True, null=True)  #User's LinkedIn profile
+    websiteUrl = models.URLField(max_length=256, blank=True, null=True)  #User's personal website
     scientific_background = models.CharField(max_length=10, default='--',
-                                             choices=SCIENTIFIC_DOMAINS) # PREDEFINED LIST. LIST "SCIENTIFIC DOMAINS"
+                                             choices=SCIENTIFIC_DOMAINS)  # PREDEFINED LIST. LIST "SCIENTIFIC DOMAINS"
 
 
 UserProfile.avatar = property(lambda d: Photo.objects.get_or_create(user_profile=d)[0])
@@ -37,10 +37,10 @@ class Photo(models.Model):
     photo_original = ImageWithThumbsField(
         upload_to=photo_upload_path,
         thumbs=(
-        ('48x48', {'size': (48, 48), 'crop': True}),
-        ('68x68', {'size': (68, 68), 'crop': True}),
-        ('100x100', {'size': (100, 100), 'crop': True}),
-        ('150x150', {'size': (150, 150), 'crop': True}),
+            ('48x48', {'size': (48, 48), 'crop': True}),
+            ('68x68', {'size': (68, 68), 'crop': True}),
+            ('100x100', {'size': (100, 100), 'crop': True}),
+            ('150x150', {'size': (150, 150), 'crop': True}),
         ),
         blank=True,
         null=True)
@@ -50,43 +50,46 @@ class Photo(models.Model):
 
 
 class Vocabulary(models.Model):
-	#General information
-	title = models.CharField(max_length=128, blank=False, null=False)
-	description = models.CharField(max_length=2048, blank=False, null=False)
-	category = models.CharField(max_length=256, blank=True, choices=CATEGORIES)  #checkbox-list, PREDEFINED LIST - LIST "CATEGORIES"
-	
-	#RDF schema properties
-	originalUrl = models.URLField(max_length=256, blank=False, null=True) #Location of the original vocabulary (for instance, in the publisher's website)
-	downloadUrl = models.URLField(max_length=256, blank=False, null=True) #Location of the original vocabulary (for instance, in the publisher's website)
-	preferredNamespaceUri = models.URLField(max_length=1024, blank=False, null=True) #Preferred namespace uri
-	preferredNamespacePrefix = models.CharField(max_length=256, blank=False, null=True) #Preffered namespace prefix
-	
-	#Logging
-	uploader = models.ForeignKey(User)
-	datePublished = models.DateField(blank=True, null=True) #Original vocabulary publish date
-	dateCreated = models.DateField(blank=True, null=True) #Vocabulary creation inside LinDa
-	dateModified = models.DateField(blank=True, null=True) #Last vocabulary modification
-	
-	#Social properties
-	score = models.IntegerField() #Sum of the votes for the vocabulary
-	votes = models.IntegerField() #Number of votes for the vocabulary 
-	downloads = models.IntegerField() #Number of downloads for the vocabulary
-	
-	def title_slug(self):
-		return slugify(self.title)
-		
-	def get_absolute_url(self):
-		return u'/vocabulary/%d/%s' % (self.id, self.title_slug())
-		
-	def has_been_voted(self):
-		return self.score > 0
-		
-	def get_score(self):
-		return float(self.score) / self.votes
+    #General information
+    title = models.CharField(max_length=128, blank=False, null=False)
+    description = models.CharField(max_length=2048, blank=False, null=False)
+    category = models.CharField(max_length=256, blank=True,
+                                choices=CATEGORIES)  #checkbox-list, PREDEFINED LIST - LIST "CATEGORIES"
 
-	def hasVoted(self, user):
-		return False
-		
+    #RDF schema properties
+    originalUrl = models.URLField(max_length=256, blank=False,
+                                  null=True)  #Location of the original vocabulary (for instance, in the publisher's website)
+    downloadUrl = models.URLField(max_length=256, blank=False,
+                                  null=True)  #Location of the original vocabulary (for instance, in the publisher's website)
+    preferredNamespaceUri = models.URLField(max_length=1024, blank=False, null=True)  #Preferred namespace uri
+    preferredNamespacePrefix = models.CharField(max_length=256, blank=False, null=True)  #Preffered namespace prefix
+
+    #Logging
+    uploader = models.ForeignKey(User)
+    datePublished = models.DateField(blank=True, null=True)  #Original vocabulary publish date
+    dateCreated = models.DateField(blank=True, null=True)  #Vocabulary creation inside LinDa
+    dateModified = models.DateField(blank=True, null=True)  #Last vocabulary modification
+
+    #Social properties
+    score = models.IntegerField()  #Sum of the votes for the vocabulary
+    votes = models.IntegerField()  #Number of votes for the vocabulary
+    downloads = models.IntegerField()  #Number of downloads for the vocabulary
+
+    def title_slug(self):
+        return slugify(self.title)
+
+    def get_absolute_url(self):
+        return u'/vocabulary/%d/%s' % (self.id, self.title_slug())
+
+    def has_been_voted(self):
+        return self.score > 0
+
+    def get_score(self):
+        return float(self.score) / self.votes
+
+    def hasVoted(self, user):
+        return False
+
 
 class VocabularyRanking(models.Model):
     voter = models.ForeignKey(User)
@@ -95,8 +98,8 @@ class VocabularyRanking(models.Model):
 
 
 class VocabularyComments(models.Model):
-    commentText = models.CharField(max_length=512, blank=False, null=False) #Comment content
-    vocabularyCommented = models.ForeignKey(Vocabulary) #Vocabulary-discussion
-    user = models.ForeignKey(User) #Comment author
-    timePosted = models.DateTimeField(blank=True, null=True) #Comment timestamp
+    commentText = models.CharField(max_length=512, blank=False, null=False)  #Comment content
+    vocabularyCommented = models.ForeignKey(Vocabulary)  #Vocabulary-discussion
+    user = models.ForeignKey(User)  #Comment author
+    timePosted = models.DateTimeField(blank=True, null=True)  #Comment timestamp
 
