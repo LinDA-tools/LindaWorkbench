556546e01e4e2fff40c0742a5cc1a3451c553e80
Dimitris Papaspyros <dipapaspyros@gmail.com>
2014-10-06 15:43:00 +0300

    Added table, properties and columns proxy calls for r2r

--- linda/r2r/views.py
+++ linda/r2r/views.py
@@ -10,7 +10,7 @@ R2R_SERVER = LINDA_SERVER_IP + ":3000/"
 R2R_PROXY = LINDA_SERVER_IP + ":8000/transformations"
 
 
-# Create your views here.
+# Transformation page view
 def transform(request):
     params = {}
     params['R2R_PROXY'] = R2R_PROXY
@@ -19,7 +19,31 @@ def transform(request):
     return render_to_response("r2r/index.html", params)
 
 
+#Proxy calls - exist as middle-mans between LinDA tranformations page and the r2r server
+
+#Get all table
 def get_tables(request):
     data = requests.get(R2R_SERVER + "api/v1/db/tables")
+
+    return HttpResponse(data, "application/json")
+
+
+#Get table specified by name
+def get_table(request):
+    data = requests.get(R2R_SERVER + "api/v1/db/tables?name=" + request.GET['name'])
+
+    return HttpResponse(data, "application/json")
+
+
+#Get all columns in table
+def get_columns(request):
+    data = requests.get(R2R_SERVER + "api/v1/db/columns?table=" + request.GET['table'])
+
+    return HttpResponse(data, "application/json")
+
+
+#Get all properties in table
+def get_properties(request):
+    data = requests.get(R2R_SERVER + "api/v1/lov/properties?table=" + request.GET['table'])
 
     return HttpResponse(data, "application/json")
