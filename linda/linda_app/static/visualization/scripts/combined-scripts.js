68974071edcfe0c119091657b0d71bbc928f037e
Christos Botsikas <cbot@epu.ntua.gr>
2014-12-01 18:17:56 +0200

    Remove hardcoded localhost hostname

--- linda/linda_app/static/visualization/scripts/combined-scripts.js
+++ linda/linda_app/static/visualization/scripts/combined-scripts.js
@@ -113,7 +113,7 @@ App.VisualizationController = Ember.ArrayController.extend({
 App.Store = DS.Store.extend({
     revision: 13,
     adapter: DS.RESTAdapter.extend({
-        host: 'http://localhost:3002'
+        host: 'http://'+window.location.hostname+':3002'
     })
 });
 
