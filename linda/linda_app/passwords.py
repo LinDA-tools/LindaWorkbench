01f1d3f512648b0457b5040ce3de04151bf22b6f
Robert Danitz <robert.danitz@fokus.fraunhofer.de>
2015-01-23 11:32:02 +0100

    clear password

--- linda/linda_app/passwords.py
+++ linda/linda_app/passwords.py
@@ -1,6 +1,6 @@
 #MySQL database credentials
-DB_USER = 'root'
-DB_PASSWORD = 'as67hj98'
+DB_USER = '{{MySQL database user}}'
+DB_PASSWORD = '{{MySQL database password}}'
 
 #Microsoft Translate credentials
 MS_TRANSLATOR_UID = '{{Microsoft translator user id}}'
