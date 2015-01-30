f827e3ddc5cec76e8cd17d8d0fbcdd20b089c991
Dimitris Papaspyros <dipapaspyros@gmail.com>
2015-01-30 17:05:12 +0200

    Added email configuration

--- linda/linda_app/settings.py
+++ linda/linda_app/settings.py
@@ -214,7 +214,15 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
+# mail configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'lindaprojectfp7@gmail.com'
+EMAIL_HOST_PASSWORD = passwords.EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'lindaprojectfp7@gmail.com'
 
 # necessarily specified under Django 1.7
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
