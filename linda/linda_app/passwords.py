7176020434a868bcd2f24dc0a30c18c5824c2fa3
Dimitris Papaspyros <dpap@epu.ntua.gr>
2015-02-16 10:56:41 +0200

    Add email password to passwords requirements

--- linda/linda_app/passwords.py
+++ linda/linda_app/passwords.py
@@ -5,3 +5,6 @@ DB_PASSWORD = '{{MySQL database password}}'
 #Microsoft Translate credentials
 MS_TRANSLATOR_UID = '{{Microsoft translator user id}}'
 MS_TRANSLATOR_SECRET = '{{Microsoft translator secret key}}'
+
+#Email password
+EMAIL_HOST_PASSWORD = '{{LinDA Email password or application token}}'
