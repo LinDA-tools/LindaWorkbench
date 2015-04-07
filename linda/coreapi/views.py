c83558d6d19d30d7a04e920fec8208389e869de9
Dimitris Papaspyros <dipapaspyros@gmail.com>
2015-04-07 17:02:07 +0300

    Coreapi fix

--- linda/coreapi/views.py
+++ linda/coreapi/views.py
@@ -28,36 +28,6 @@ def api_datasources_list(request):
 
 
 @csrf_exempt
-def api_datasources_list(request):
-    results = []
-    for source in DatasourceDescription.objects.all():
-        source_info = {}
-        source_info['name'] = source.name
-        source_info['uri'] = source.uri
-        source_info['title'] = source.title
-        results.append(source_info)
-
-    data = json.dumps(results)
-    mimetype = 'application/json'
-    return HttpResponse(data, mimetype)
-
-
-@csrf_exempt
-def api_datasources_list(request):
-    results = []
-    for source in DatasourceDescription.objects.all():
-        source_info = {}
-        source_info['name'] = source.name
-        source_info['uri'] = source.uri
-        source_info['title'] = source.title
-        results.append(source_info)
-
-    data = json.dumps(results)
-    mimetype = 'application/json'
-    return HttpResponse(data, mimetype)
-
-
-@csrf_exempt
 def recommend_dataset(request):
     vocabulary = request.GET.get("vocabulary", None)
     class_ = request.GET.get("class", None)
