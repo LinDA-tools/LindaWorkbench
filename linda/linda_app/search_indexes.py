7cb7c0f1d8aa208f70b03458042b760730de3401
dipapaspyros <dpap@epu.ntua.gr>
2015-10-05 12:01:50 +0300

    Improved search results

--- linda/linda_app/search_indexes.py
+++ linda/linda_app/search_indexes.py
@@ -5,11 +5,11 @@ from haystack import indexes
 from linda_app.models import Vocabulary, VocabularyClass, VocabularyProperty
 
 
+# In haystack backend settings change default min_gram to 4-5 for optimal results
 class VocabularyIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='title')
-    title_auto = indexes.EdgeNgramField(model_attr='title')
-    description = indexes.CharField(model_attr='description')
+    text = indexes.NgramField(document=True, use_template=True)
+    title = indexes.NgramField(model_attr='title')
+    description = indexes.NgramField(model_attr='description')
     preferredNamespacePrefix = indexes.CharField(model_attr='preferredNamespacePrefix')
 
     def get_model(self):
@@ -17,16 +17,16 @@ class VocabularyIndex(indexes.SearchIndex, indexes.Indexable):
 
 
 class VocabularyClassIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    label = indexes.CharField(model_attr='label')
+    text = indexes.NgramField(document=True, use_template=True)
+    label = indexes.NgramField(model_attr='label')
 
     def get_model(self):
         return VocabularyClass
 
 
 class VocabularyPropertyIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    label = indexes.CharField(model_attr='label')
+    text = indexes.NgramField(document=True, use_template=True)
+    label = indexes.NgramField(model_attr='label')
 
     def get_model(self):
         return VocabularyProperty
