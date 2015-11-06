8893939ad14b28d0661455c6078da559caa11519
dipapaspyros <dpap@epu.ntua.gr>
2015-11-06 12:30:10 +0200

    Removed docker from main project

--- docker/all-commands.sh
+++
@@ -1,4 +0,0 @@
-/tomcat-run.sh &
-/elasticsearch/bin/elasticsearch &
-service mysql start
-python3 /LinDAWorkbench/linda/manage.py runserver
--- docker/qdmodels.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Design(models.Model):
-    data = models.TextField(blank=False, null=False)  # the json data required to rebuild the design
